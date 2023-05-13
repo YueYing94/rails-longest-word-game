1c418952373e3272d7f57d72d579dc9716dcc9ba
yue ying <sarahyingyue1994@gmail.com>
2023-05-13 11:25:05 +0200

    longest word

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,19 +1,24 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
     @letters = Array.new(10) { ('A'..'Z').to_a.sample }
   end
 
   def score
-    answer = params[:result]
-    english_word = JSON.parse(URI.open("https://wagon-dictionary.herokuapp.com/#{answer}").read)["found"]
-    array = []
-    @letters.each { |letter| array >> letter if answer.include? letter }
-    if array.length =! answer.length
-      "Sorry but #{answer} can't be bult out of #{@letters.join(",")}"
-    elsif english_word!
-      "Sorry but #{answer.upcase} does not seem to be a valid English word"
+    @letters = params[:letters]  #string
+    @answer = params[:result].upcase.split("") #array
+    english_word = JSON.parse(URI.open("https://wagon-dictionary.herokuapp.com/#{params[:result]}").read)["found"]
+    include = params[:result].upcase.chars.all? { |letter| params[:result].upcase.count(letter) <= @letters.split(" ").count(letter) }
+    if include
+      if english_word
+        @result = "Congratulations! #{@answer.join("")} is a valid English word"
+      else
+        @result = "Sorry but #{@answer.join("")} does not seem to be a valid English word"
+      end
     else
-      "Congratulations! #{answer.upcase} is a valid English word"
+      @result = "Sorry but #{@answer} can't be bult out of #{@letters}"
     end
   end
 end
